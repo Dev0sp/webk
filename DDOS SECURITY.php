dd09aa6880ac510e5716e6f61bf37c7602a9c7af
DevOps | Newbies | Somethin Unique <Shivvthakur1983@gmail.com>
2025-06-09 17:56:03 +0530

    Add files via upload

---
+++ DDOS SECURITY.php
@@ -0,0 +1,25 @@
+<?php
+$ip = $_SERVER['REMOTE_ADDR'];
+$requests = array();
+$block_time = 10;
+
+function checkIP($ip) {
+    global $block_time, $requests;
+
+    foreach ($requests as $key => $value) {
+        if ($value < time()) {
+            unset($requests[$key]);
+        }
+    }
+
+    if (count($requests) >= 20) {
+        http_response_code(403);
+        exit("THERE IS DDOS SECURITY ");
+        
+    }
+
+    $requests[$ip] = time() + $block_time;
+}
+
+checkIP($ip);
+?>
